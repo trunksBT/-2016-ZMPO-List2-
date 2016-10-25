487277e8434152b676044df8e785f5e5a899869f
korytkoBT <209826@student.pwr.wroc.pl>
2016-10-25 01:28:38 +0000

    Repair goHandler

--- CShapes/CShapes/Handlers/GoHandler.cpp
+++ CShapes/CShapes/Handlers/GoHandler.cpp
@@ -109,12 +109,7 @@ CODE CGoHandler::purePerform(CPointWithSize inCache)
         for (int i = 0; i < idxOrAmount; i++, cursor++)
         {
             CFlyweight::updateIsInitializedPointCache(cursor, true);
-            newTable[cursor] = CRectangle::buildNewObj(
-                DEFAULT_FST_AXIS_VAL,
-                DEFAULT_FST_AXIS_VAL,
-                DEFAULT_SND_AXIS_VAL,
-                DEFAULT_SND_AXIS_VAL
-            );
+            newTable[cursor] = CPoint::buildNewObj(DEFAULT_FST_AXIS_VAL, DEFAULT_FST_AXIS_VAL);
         }
         CFlyweight::setPointCacheSize(newSize);
         CFlyweight::updatePointCache(newTable);
