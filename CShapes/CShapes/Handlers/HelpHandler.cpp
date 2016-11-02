bfc81ac26f18c7725a6d957202c430abaad26d4f
korytkoBT <209826@student.pwr.wroc.pl>
2016-11-02 19:50:09 +0000

    Add not implemented HelpHandler

--- CShapes/CShapes/Handlers/HelpHandler.cpp
+++ CShapes/CShapes/Handlers/HelpHandler.cpp
@@ -1,35 +1,72 @@
 #include "stdafx.h"
 #include <iostream>
 
-//#include "HelpHandler.h"
-//#include "../Utils.hpp"
-//#include "../Point.hpp"
-//#include "../Flyweight.h"
-//
-//using namespace defaultVals;
-//
-//using namespace funs;
-//
-//CHelpHandler::CHelpHandler(std::vector<std::string>& inCommand)
-//    : IPointHandler(inCommand)
-//{}
-//
-//const int CHelpHandler::getProperAmountOfArgs()
-//{
-//    return 1;
-//}
-//
-//std::string CHelpHandler::getProperTypesOfArgs()
-//{
-//    return "s";
-//}
-//
-//CODE CHelpHandler::purePerform(std::vector<CPointWithSize>& inCache)
-//{
-//    for(int i = 0; i < fullCommands::allCommands.size(); i++)
-//    {
-//        std::cout << fullCommands::allCommands[i] << POST_PRINT;
-//    }
-//
-//    return CODE::DONE;
-//}
+#include "HelpHandler.h"
+#include "../Utils.hpp"
+#include "../Point.hpp"
+#include "../Flyweight.h"
+
+using namespace defaultVals;
+using namespace funs;
+using namespace cacheIdx;
+using namespace tupleIdx;
+
+CHelpHandler::CHelpHandler(std::vector<std::string>& inCommand)
+    : IHandler(inCommand), IShapeHandler(inCommand)
+{}
+
+const int CHelpHandler::getProperAmountOfArgs()
+{
+    return 3;
+}
+
+std::string CHelpHandler::getProperTypesOfArgs()
+{
+    return "sii";
+}
+
+CODE CHelpHandler::checkArgsAndPerform(CShapeWithSize inPointCache)
+{
+    if (IHandler::checkTypeAndAmountOfArgs() == CODE::DONE)
+    {
+        return purePerform(inPointCache);
+    }
+    else
+    {
+        return CODE::ERROR;
+    }
+}
+
+
+CODE CHelpHandler::purePerform(CShapeWithSize inCache)
+{
+    std::string copyToIdStr(wholeCommand_[idxOf::COPY_TO]);
+    int copyToId = std::stoi(copyToIdStr);
+
+    std::string copyFromIdStr(wholeCommand_[idxOf::COPY_FROM]);
+    int copyFromId = std::stoi(copyFromIdStr);
+
+    if (!isProperIdx(copyToId, std::get<SIZE>(inCache)) ||
+        !isProperIdx(copyFromId, std::get<SIZE>(inCache)))
+    {
+        return CODE::ERROR;
+    }
+
+    if (copyToId == copyFromId)
+    {
+        return CODE::DONE;
+    }
+
+    delete std::get<ARRAY>(inCache)[copyToId];
+    std::get<ARRAY>(inCache)[copyToId] =
+        CRectangle::buildNewObj(
+            reinterpret_cast<CRectangle*>(std::get<ARRAY>(inCache)[copyFromId]));
+
+    return CODE::DONE;
+}
+
+CHelpHandler::~CHelpHandler()
+{
+
+}
+
